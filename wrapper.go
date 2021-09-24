06f8efbafb2640e4cd4c71ebfd8804e380107198
ebarti <eloi.barti@glovoapp.com>
2021-09-24 21:06:20 +0200

    More WIP

--- wrapper.go
+++ wrapper.go
@@ -32,12 +32,36 @@ type IncidentServicesAPI interface {
 }
 
 type IncidentTeamsAPI interface {
+	CreateIncidentTeam(ctx context.Context, body IncidentTeamCreateRequest) (IncidentTeamResponse, *http.Response, error)
+	DeleteIncidentTeam(ctx context.Context, teamId string) (*http.Response, error)
+	GetIncidentTeam(ctx context.Context, teamId string, o ...GetIncidentTeamOptionalParameters) (IncidentTeamResponse, *http.Response, error)
+	ListIncidentTeams(ctx context.Context, o ...ListIncidentTeamsOptionalParameters) (IncidentTeamsResponse, *http.Response, error)
+	UpdateIncidentTeam(ctx context.Context, teamId string, body IncidentTeamUpdateRequest) (IncidentTeamResponse, *http.Response, error)
 }
 
 type IncidentsAPI interface {
+	CreateIncident(ctx context.Context, body IncidentCreateRequest) (IncidentResponse, *http.Response, error)
+	DeleteIncident(ctx context.Context, incidentId string) (*http.Response, error)
+	GetIncident(ctx context.Context, incidentId string, o ...GetIncidentOptionalParameters) (IncidentResponse, *http.Response, error)
+	ListIncidents(ctx context.Context, o ...ListIncidentsOptionalParameters) (IncidentsResponse, *http.Response, error)
+	UpdateIncident(ctx context.Context, incidentId string, body IncidentUpdateRequest) (IncidentResponse, *http.Response, error)
 }
 
 type KeyManagementAPI interface {
+	CreateAPIKey(ctx context.Context, body APIKeyCreateRequest) (APIKeyResponse, *http.Response, error)
+	CreateCurrentUserApplicationKey(ctx context.Context, body ApplicationKeyCreateRequest) (ApplicationKeyResponse, *http.Response, error)
+	DeleteAPIKey(ctx context.Context, apiKeyId string) (*http.Response, error)
+	DeleteApplicationKey(ctx context.Context, appKeyId string) (*http.Response, error)
+	DeleteCurrentUserApplicationKey(ctx context.Context, appKeyId string) (*http.Response, error)
+	GetAPIKey(ctx context.Context, apiKeyId string, o ...GetAPIKeyOptionalParameters) (APIKeyResponse, *http.Response, error)
+	GetApplicationKey(ctx context.Context, appKeyId string, o ...GetApplicationKeyOptionalParameters) (ApplicationKeyResponse, *http.Response, error)
+	GetCurrentUserApplicationKey(ctx context.Context, appKeyId string) (ApplicationKeyResponse, *http.Response, error)
+	ListAPIKeys(ctx context.Context, o ...ListAPIKeysOptionalParameters) (APIKeysResponse, *http.Response, error)
+	ListApplicationKeys(ctx context.Context, o ...ListApplicationKeysOptionalParameters) (ListApplicationKeysResponse, *http.Response, error)
+	ListCurrentUserApplicationKeys(ctx context.Context, o ...ListCurrentUserApplicationKeysOptionalParameters) (ListApplicationKeysResponse, *http.Response, error)
+	UpdateAPIKey(ctx context.Context, apiKeyId string, body APIKeyUpdateRequest) (APIKeyResponse, *http.Response, error)
+	UpdateApplicationKey(ctx context.Context, appKeyId string, body ApplicationKeyUpdateRequest) (ApplicationKeyResponse, *http.Response, error)
+	UpdateCurrentUserApplicationKey(ctx context.Context, appKeyId string, body ApplicationKeyUpdateRequest) (ApplicationKeyResponse, *http.Response, error)
 }
 
 type LogsAPI interface {
@@ -60,22 +84,34 @@ type LogsArchivesAPI interface {
 }
 
 type LogsMetricsAPI interface {
+	CreateLogsMetric(ctx context.Context, body LogsMetricCreateRequest) (LogsMetricResponse, *http.Response, error)
+	DeleteLogsMetric(ctx context.Context, metricId string) (*http.Response, error)
+	GetLogsMetric(ctx context.Context, metricId string) (LogsMetricResponse, *http.Response, error)
+	ListLogsMetrics(ctx context.Context) (LogsMetricsResponse, *http.Response, error)
+	UpdateLogsMetric(ctx context.Context, metricId string, body LogsMetricUpdateRequest) (LogsMetricResponse, *http.Response, error)
 }
 
 type MetricsAPI interface {
+	CreateTagConfiguration(ctx context.Context, metricName string, body MetricTagConfigurationCreateRequest) (MetricTagConfigurationResponse, *http.Response, error)
+	DeleteTagConfiguration(ctx context.Context, metricName string) (*http.Response, error)
+	ListTagConfigurationByName(ctx context.Context, metricName string) (MetricTagConfigurationResponse, *http.Response, error)
+	ListTagConfigurations(ctx context.Context, o ...ListTagConfigurationsOptionalParameters) (MetricsAndMetricTagConfigurationsResponse, *http.Response, error)
+	ListTagsByMetricName(ctx context.Context, metricName string) (MetricAllTagsResponse, *http.Response, error)
+	ListVolumesByMetricName(ctx context.Context, metricName string) (MetricVolumesResponse, *http.Response, error)
+	UpdateTagConfiguration(ctx context.Context, metricName string, body MetricTagConfigurationUpdateRequest) (MetricTagConfigurationResponse, *http.Response, error)
 }
 
 type ProcessesAPI interface {
 }
 
-type a3 interface {
+type RolesAPI interface {
 }
 
-type a3 interface {
+type SecurityMonitoringAPI interface {
 }
 
-type a3 interface {
+type ServiceAccountsAPI interface {
 }
 
-type a3 interface {
+type UsersAPI interface {
 }
